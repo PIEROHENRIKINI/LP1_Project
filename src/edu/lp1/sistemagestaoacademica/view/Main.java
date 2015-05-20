47ed3c67d1aa34a7076b94e887670db83e0edfc7
Brendoon Ryos <brendoon-ryos@live.com>
2015-05-19 23:30:53 -0300

    Add Image

--- src/edu/lp1/sistemagestaoacademica/view/Main.java
+++ src/edu/lp1/sistemagestaoacademica/view/Main.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Color;
+import java.awt.Toolkit;
 
 public class Main extends JFrame {
 
@@ -33,6 +34,7 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("/home/bryos/Documentos/graduation-cap-icon_153875879.jpg"));
 		setTitle("Sistema de Gestão Acadêmica");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
